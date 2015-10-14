fb4fbdbefc3cf85d5ea0b69683e7f4475832ced4
dylanmontandon <dylan.montando@IG14-MONTANDOD.intra.eiaj.ch>
2015-10-14 22:10:40 +0200

    fixed block

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,41 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 $(document).ready(function(){
 	'use strict';
 	$('a').smoothScroll();
+	
+	
+	function fixDiv() {
+    var $cache = $('#getFixed');
+    if ($(window).scrollTop() > 100)
+      $cache.css({
+        'position': 'fixed',
+        'top': '10px'
+      });
+    else
+      $cache.css({
+        'position': 'fixed',
+        'top': 'auto'
+      });
+  }
+  
+  $(window).scroll(fixDiv);
+  
+  function wResize() {
+         
+             var winW = $(window).width();
+         
+         if ( winW < '1500') {
+             $("#getFixed").hide(); 
+         }
+         else {
+             $("#getFixed").show(); 
+         }         
+             
+     }
+    
+     wResize();
+    
+    $(window).resize(function() {
+        wResize();
+    });
+  
 });
