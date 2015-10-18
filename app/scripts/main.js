9b1c8009d48c78c63b4b7549e1b1948859a0c713
dylanmontandon <dylan.montando@IG14-MONTANDOD.intra.eiaj.ch>
2015-10-18 21:28:22 +0200

    nanogallery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,20 +4,25 @@ $(document).ready(function(){
 	$('a').smoothScroll();
 	
   function wResize() {
-         
-             var winW = $(window).width();
-         
-         if ( winW < '1500') {
-             $("#getFixed").hide(); 
-         }
-         else {
-             $("#getFixed").show(); 
-         }         
-             
-     }
-    
-    $(window).resize(function() {
-        wResize();
-    });
+        var winW = $(window).width();
+        
+        if ( winW < '1500') {
+            $("#getFixed").hide(); 
+        }
+        else {
+            $("#getFixed").show(); 
+        }         
+            
+    }
+   
+   $(window).resize(function() {
+       wResize();
+   });
   
+  $("#diplome").nanoGallery({
+        itemsBaseURL:'./images',
+		paginationMaxLinesPerPage: 1,
+		thumbnailWidth: 165,
+        thumbnailHeight: 110,
+  });
 });
